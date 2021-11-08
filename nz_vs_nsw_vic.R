d20e5a46b8d8c6792ba45f29fae61981ed51fed7
Aaron Schiff <aaron@schiff.nz>
2021-11-09 09:56:07 +1300

    improvements to nz vs au delta outbreak comparisons

--- nz_vs_nsw_vic.R
+++ nz_vs_nsw_vic.R
@@ -9,6 +9,7 @@ library(lubridate)
 library(janitor)
 library(scales)
 library(readxl)
+library(RcppRoll)
 
 # *****************************************************************************
 
@@ -18,7 +19,7 @@ library(readxl)
 
 # Case data files
 dat_nsw <- read_csv(file = here("data/confirmed_cases_table1_location.csv"))   # https://data.nsw.gov.au/search/dataset/ds-nsw-ckan-aefcde60-3b0c-4bc0-9af1-6fe652944ec2/details?q=
-dat_nz <- read_csv(file = here("data/covid_cases_2021-11-04.csv")) |>   # https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-case-demographics
+dat_nz <- read_csv(file = here("data/covid_cases_2021-11-08.csv")) |>   # https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-case-demographics
   clean_names()
 dat_vic <- read_csv(file = here("data/ncov-covid-cases-by-lga-csv.csv")) |>   # https://www.coronavirus.vic.gov.au/victorian-coronavirus-covid-19-data
   clean_names()
@@ -95,100 +96,8 @@ dat_nz_delta_l3 <- dat_nz |>
 # *****************************************************************************
 # Visualise ----
 
-# Chart of NZ vs NSW and VIC full delta outbreak
-chart1_dat <- bind_rows(
-  # NSW
-  dat_nsw_delta |> 
-    mutate(area = "nsw") |> 
-    left_join(y = dat_nsw_vax |> 
-                select(outbreak_day, fully_vax_rate), 
-              by = "outbreak_day"), 
-  
-  # VIC 
-  dat_vic_delta |> 
-    mutate(area = "vic") |> 
-    left_join(y = dat_vic_vax |> 
-                select(outbreak_day, fully_vax_rate), 
-              by = "outbreak_day"), 
-  
-  # NZ
-  dat_nz_delta |> 
-    mutate(area = "nz") |> 
-    left_join(y = dat_nz_vax |> 
-                select(delta_day, fully_vax_rate), 
-              by = c("outbreak_day" = "delta_day"))
-) |> 
-  mutate(area = factor(x = area, 
-                       levels = c("nsw", 
-                                  "vic", 
-                                  "nz"), 
-                       labels = c("New South Wales since 16 June", 
-                                  "Victoria since 5 August", 
-                                  "NZ since 17 August"), 
-                       ordered = TRUE))
-
-chart1 <- chart1_dat |> 
-  ggplot(mapping = aes(x = outbreak_day, 
-                       y = n, 
-                       colour = area)) + 
-  geom_line(size = 1.25) + 
-  xlab("Outbreak day") + 
-  ylab("Number of\ncases reported") + 
-  scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
-                                 "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 17 August" = "cornflowerblue"), 
-                      name = NULL) + 
-  scale_x_continuous(breaks = seq(0, 200, 10)) + 
-  scale_y_continuous(breaks = seq(0, 2600, 200), 
-                     labels = comma_format(accuracy = 1)) + 
-  theme_minimal(base_family = "Fira Sans") + 
-  theme(panel.grid.minor = element_blank(), 
-        panel.grid.major = element_line(size = 0.2), 
-        axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
-        axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.21, 0.92))
-
-ggsave(filename = here("outputs/nz_vs_nsw_vic_1.png"), 
-       plot = chart1, 
-       width = 2400, 
-       height = 1600, 
-       units = "px", 
-       device = "png", 
-       bg = "white")
-
-# Chart of NZ vs NSW full delta outbreak vs vaccination rate
-chart1_with_vax <- chart1_dat |> 
-  ggplot(mapping = aes(x = fully_vax_rate, 
-                       y = n, 
-                       colour = area)) + 
-  geom_line(size = 1.25) + 
-  xlab("Fully vaccinated (% of total population)") + 
-  ylab("Number of\ncases reported") + 
-  scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
-                                 "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 17 August" = "cornflowerblue"), 
-                      name = NULL) + 
-  scale_x_continuous(breaks = seq(0, 0.8, 0.1), 
-                     labels = percent_format(accuracy = 1)) + 
-  scale_y_continuous(breaks = seq(0, 2400, 200), 
-                     labels = comma_format(accuracy = 1)) + 
-  theme_minimal(base_family = "Fira Sans") + 
-  theme(panel.grid.minor = element_blank(), 
-        panel.grid.major = element_line(size = 0.2), 
-        axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
-        axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.2, 0.92))
-
-ggsave(filename = here("outputs/nz_vs_nsw_vic_1_with_vax.png"), 
-       plot = chart1_with_vax, 
-       width = 2400, 
-       height = 1600, 
-       units = "px", 
-       device = "png", 
-       bg = "white")
-
 # Chart of NZ from level 3 vs NSW and VIC full delta outbreak
-chart2_dat <- bind_rows(
+chart_outbreak_day_dat <- bind_rows(
   # NSW
   dat_nsw_delta |> 
     mutate(area = "nsw") |> 
@@ -215,19 +124,27 @@ chart2_dat <- bind_rows(
                                   "nz"), 
                        labels = c("New South Wales since 16 June", 
                                   "Victoria since 5 August", 
-                                  "NZ since 22 September"), 
+                                  "NZ since 22 September (Auckland AL3)"), 
                        ordered = TRUE))
 
-chart2 <- chart2_dat |> 
+chart_outbreak_day <- chart_outbreak_day_dat |> 
   ggplot(mapping = aes(x = outbreak_day, 
                        y = n, 
                        colour = area)) + 
   geom_line(size = 1.25) + 
   xlab("Outbreak day") + 
-  ylab("Number of\ncases reported") + 
+  ylab("Daily number of\ncases reported") + 
+  ggtitle(label = "Delta outbreak daily cases") + 
+  annotate(geom = "text", 
+           x = 85, 
+           y = 2, 
+           label = "Chart by Aaron Schiff\nData sources: health.govt.nz, data.nsw.gov.au, coronavirus.vic.gov.au\ngithub.com/aaronschiff/nz-covid", 
+           hjust = 0, 
+           family = "Fira Sans", 
+           size = 2) + 
   scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
                                  "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 22 September" = "cornflowerblue"), 
+                                 "NZ since 22 September (Auckland AL3)" = "cornflowerblue"), 
                       name = NULL) + 
   scale_x_continuous(breaks = seq(0, 200, 10)) + 
   scale_y_continuous(breaks = seq(0, 2400, 200), 
@@ -237,55 +154,76 @@ chart2 <- chart2_dat |>
         panel.grid.major = element_line(size = 0.2), 
         axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
         axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.2, 0.92))
+        legend.position = c(0.22, 0.9))
 
-ggsave(filename = here("outputs/nz_vs_nsw_vic_2.png"), 
-       plot = chart2, 
+ggsave(filename = here("outputs/nz-vs-au/nz_vs_nsw_vic_by_outbreak_day.png"), 
+       plot = chart_outbreak_day, 
        width = 2400, 
        height = 1600, 
        units = "px", 
        device = "png", 
        bg = "white")
 
-chart2_log_scale <- chart2_dat |> 
+chart_outbreak_day_mean_log <- chart_outbreak_day_dat |> 
+  group_by(area) |> 
+  mutate(mean_n = roll_mean(x = n, n = 5L, align = "right", fill = NA_real_)) |> 
   ggplot(mapping = aes(x = outbreak_day, 
-                       y = n, 
+                       y = mean_n, 
                        colour = area)) + 
   geom_line(size = 1.25) + 
   xlab("Outbreak day") + 
-  ylab("Number of\ncases reported") + 
+  ylab("Daily number of\ncases reported\n(log scale)") + 
+  ggtitle(label = "Delta outbreak daily cases: 5-day moving average (log scale)") + 
+  annotate(geom = "text", 
+           x = 82, 
+           y = 2, 
+           label = "Chart by Aaron Schiff\nData sources: health.govt.nz, data.nsw.gov.au, coronavirus.vic.gov.au\ngithub.com/aaronschiff/nz-covid", 
+           hjust = 0, 
+           family = "Fira Sans", 
+           size = 2) + 
   scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
                                  "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 22 September" = "cornflowerblue"), 
+                                 "NZ since 22 September (Auckland AL3)" = "cornflowerblue"), 
                       name = NULL) + 
   scale_x_continuous(breaks = seq(0, 200, 10)) + 
-  scale_y_log10(labels = comma_format(accuracy = 1)) + 
+  scale_y_log10(labels = comma_format(accuracy = 1), 
+                breaks = c(1, 10, 100, 1000), 
+                limits = c(1, 2500)) + 
   theme_minimal(base_family = "Fira Sans") + 
   theme(panel.grid.minor = element_blank(), 
         panel.grid.major = element_line(size = 0.2), 
         axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
         axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.2, 0.92))
+        legend.position = c(0.765, 0.3))
 
-ggsave(filename = here("outputs/nz_vs_nsw_vic_2_log.png"), 
-       plot = chart2_log_scale, 
+ggsave(filename = here("outputs/nz-vs-au/nz_vs_nsw_vic_by_outbreak_day_mean_log.png"), 
+       plot = chart_outbreak_day_mean_log, 
        width = 2400, 
        height = 1600, 
        units = "px", 
        device = "png", 
        bg = "white")
 
-# Chart of NZ from level 3 vs NSW full delta outbreak vs vaccination rate
-chart2_with_vax <- chart2_dat |> 
+# Chart of NZ from level 3 vs NSW and VIC full delta outbreak vs vaccination rate
+chart_outbreak_day_vax <- chart_outbreak_day_dat |> 
   ggplot(mapping = aes(x = fully_vax_rate, 
                        y = n, 
                        colour = area)) + 
-  geom_line(size = 1.25) + 
+  geom_point(size = 0.5) + 
+  geom_line(size = 0.3) + 
   xlab("Fully vaccinated (% of total population)") + 
   ylab("Daily number of\ncases reported") + 
+  ggtitle(label = "Delta outbreak daily cases vs vaccination rate") + 
+  annotate(geom = "text", 
+           x = 0.05, 
+           y = 1700, 
+           label = "Chart by Aaron Schiff\nData sources: health.govt.nz, data.nsw.gov.au, coronavirus.vic.gov.au\ngithub.com/aaronschiff/nz-covid", 
+           hjust = 0, 
+           family = "Fira Sans", 
+           size = 2) + 
   scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
                                  "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 22 September" = "cornflowerblue"), 
+                                 "NZ since 22 September (Auckland AL3)" = "cornflowerblue"), 
                       name = NULL) + 
   scale_x_continuous(breaks = seq(0, 0.8, 0.1), 
                      labels = percent_format(accuracy = 1)) + 
@@ -296,42 +234,14 @@ chart2_with_vax <- chart2_dat |>
         panel.grid.major = element_line(size = 0.2), 
         axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
         axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.2, 0.92))
+        legend.position = c(0.26, 0.9))
 
-ggsave(filename = here("outputs/nz_vs_nsw_vic_2_with_vax.png"), 
-       plot = chart2_with_vax, 
+ggsave(filename = here("outputs/nz-vs-au/nz_vs_nsw_vic_2_with_vax.png"), 
+       plot = chart_outbreak_day_vax, 
        width = 2400, 
        height = 1600, 
        units = "px", 
        device = "png", 
        bg = "white")
 
-chart2_with_vax_with_log <- chart2_dat |> 
-  ggplot(mapping = aes(x = fully_vax_rate, 
-                       y = n, 
-                       colour = area)) + 
-  geom_line(size = 1.25) + 
-  xlab("Fully vaccinated (% of total population)") + 
-  ylab("log(Daily number of\ncases reported)") + 
-  scale_colour_manual(values = c("New South Wales since 16 June" = grey(0.35), 
-                                 "Victoria since 5 August" = grey(0.7), 
-                                 "NZ since 22 September" = "cornflowerblue"), 
-                      name = NULL) + 
-  scale_x_continuous(breaks = seq(0, 0.8, 0.1), 
-                     labels = percent_format(accuracy = 1)) + 
-  scale_y_log10(labels = comma_format(accuracy = 1)) + 
-  theme_minimal(base_family = "Fira Sans") + 
-  theme(panel.grid.minor = element_blank(), 
-        panel.grid.major = element_line(size = 0.2), 
-        axis.title.y = element_text(angle = 0, hjust = 0, margin = margin(0, 8, 0, 0, "pt")), 
-        axis.title.x = element_text(margin = margin(8, 0, 0, 0, "pt")), 
-        legend.position = c(0.2, 0.92))
-
-ggsave(filename = here("outputs/nz_vs_nsw_vic_2_with_vax_log.png"), 
-       plot = chart2_with_vax_with_log, 
-       width = 2400, 
-       height = 1600, 
-       units = "px", 
-       device = "png", 
-       bg = "white")
-
+# *****************************************************************************
