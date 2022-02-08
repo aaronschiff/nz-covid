4eabf1fc185a9968fd7f927fe05f907c6edb0626
Aaron Schiff <aaron@schiff.nz>
2022-02-09 09:10:13 +1300

    more noodling on the MIQ chart

--- miq.R
+++ miq.R
@@ -48,20 +48,16 @@ dat_cases <- read_csv(file = here("data/covid_cases_2022-02-05.csv")) |>
   mutate(x = row_number() + 0.5) |> 
   ungroup() |> 
   mutate(x = ifelse(type == "miq", -x, x)) 
-  # count(report_date, type) |> 
-  # complete(report_date = seq(from = min(report_date), 
-  #                            to = max(report_date), 
-  #                            by = "1 day"), 
-  #          type = c("miq", "community"), 
-  #          fill = list(n = 0L)) 
 
+# Dates sequence
 dates <- tibble(
   date = seq(from = ymd("2020-01-01"), 
-             to = max(dat_cases$report_date), 
+             to = ymd("2021-12-31"), 
              by = "1 day")
 ) |> 
   mutate(date_count = row_number() - 1)
 
+# Date labels for chart
 dates_labels <- dates |> 
   filter(day(date) == 1L) |> 
   mutate(month_label = month.abb[month(date)]) |> 
@@ -69,8 +65,28 @@ dates_labels <- dates |>
                         paste(year(date), month_label), 
                         month_label))
 
+# Alert levels data 
+dat_al <- tribble(
+  ~start_date, ~end_date, ~al, ~al_text, 
+  ymd("2020-03-26"), ymd("2020-04-27"), 4L, "Alert Level 4", 
+  ymd("2020-04-28"), ymd("2020-05-13"), 3L, "Alert Level 3", 
+  ymd("2020-05-14"), ymd("2020-06-08"), 2L, "Alert Level 2", 
+  ymd("2020-06-09"), ymd("2020-08-11"), 1L, "Alert Level 1", 
+  ymd("2020-08-12"), ymd("2020-08-30"), 3L, "Alert Level 3 (Auckland) / 2 (elsewhere)", 
+  ymd("2020-08-31"), ymd("2020-09-21"), 2L, "Alert Level 2.5 (Auckland) / 2 (elsewhere)", 
+  ymd("2020-09-22"), ymd("2020-10-06"), 2L, "Alert Level 2.5 & 2 (Auckland) / 1 (elsewhere)", 
+  ymd("2020-10-07"), ymd("2021-02-14"), 1L, "Alert Level 1", 
+  ymd("2021-02-15"), ymd("")
+)
+
+# *****************************************************************************
+
+
+# *****************************************************************************
+# Chart ---- 
 
 dat_cases |> 
+  filter(year(report_date) < 2022) |> 
   ggplot(mapping = aes(y = date_count, 
                        x = x, 
                        fill = type)) + 
@@ -108,3 +124,4 @@ dat_cases |>
   theme(panel.grid.minor = element_blank())
 
 
+# *****************************************************************************
